b97e78c57aac6edb70f8d0722b15407592d9da58
wilkpiotr <piotrwilk@interia.pl>
2018-03-19 20:41:38 +0100

    using style and dataset fucntion

--- src/index.js
+++ src/index.js
@@ -4,3 +4,50 @@ window.highlight = (element) => {
 }
 
 window.h = window.highlight;
+
+window.toggleClass = (element, className) => {
+  if (element.classList.contains(className)) {
+    element.classList.remove(className);
+  } else {
+    element.classList.add(className);
+  }
+};
+
+window.setPlayersStyle = () => {
+  const players = document.querySelectorAll('li[data-type="player"]');
+  const playersArr = Array.from(players);
+  playersArr.forEach((el) => {
+    el.style.background = 'green';
+    el.style.color = 'white';
+  });
+}
+
+window.setAliensStyle = () => {
+  const aliens = document.querySelectorAll('li[data-type="alien"]');
+  const aliensArr = Array.from(aliens);
+  aliensArr.forEach((el) => {
+    el.style.background = 'orange';
+    el.style.color = 'red';
+  });
+}
+
+window.setAllStyle = () => {
+  window.setPlayersStyle();
+  window.setAliensStyle();
+}
+
+window.setWantedBgColor = (type, background) => {
+  const elements = document.querySelectorAll(`li[data-type='${type}']`);
+  // const elements = document.querySelectorAll('li[data-type="' + type +'"]');
+  const elementsArr = Array.from(elements);
+  elementsArr.forEach((el) => {
+el.style.background = background;
+  });
+}
+
+window.pandominium = () => {
+  window.setWantedBgColor('player', 'brown');
+  window.setWantedBgColor('alien', 'black');
+  window.setWantedBgColor('predator', 'green')
+  window.setWantedBgColor('other', 'red')
+}
